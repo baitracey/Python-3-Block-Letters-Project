10c8a040540b0ec3025c14cb91928bdaa8ec0147
Bailey Tracey <baileytracey2003@gmail.com>
2025-04-28 17:41:00 +0100

    printed letters of first name

--- Python 3 Block Letters Project/Python_3_Block_Letters_Project.py
+++ Python 3 Block Letters Project/Python_3_Block_Letters_Project.py
@@ -1,1 +1,53 @@
+#Bailey Tracey
+#I have a lot of tattoos
 
+print ("""
+B B B B
+B       B
+B       B
+B B B B 
+B       B
+B       B
+B B B B
+
+    A
+ A    A
+A       A
+A A A A A
+A       A
+A       A
+A       A
+
+I I I I I
+    I
+    I
+    I
+    I
+    I
+I I I I I
+
+L
+L
+L
+L
+L
+L
+L L L L L 
+
+E E E E E
+E
+E
+E E E
+E
+E
+E E E E E
+
+Y       Y
+ Y     Y
+    Y
+    Y
+    Y
+    Y
+    Y
+
+""")
